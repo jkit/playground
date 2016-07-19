dcef10c715b38c44392f598ae44b13c9c3229c53
Jimmy Kittiyachavalit <jkit@fb.com>
2016-07-19 13:25:21 -0700

    Apples and bananas

--- test.rb
+++ test.rb
@@ -1,7 +1,11 @@
 require "minitest/autorun"
 
 describe "Simple" do
-  it 'works' do
-    assert false, 'Forced failure'
+  it 'apples' do
+    assert true
+  end
+
+  it 'bananas' do
+    assert false, 'failed bananas'
   end
 end
